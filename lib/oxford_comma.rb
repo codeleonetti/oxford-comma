ed288afb0f4864382867d0965a7ed2f763c87724
john <john.d.leonetti@gmail.com>
2021-02-24 14:45:08 -0800

    commit tree ef78f920aba202eb3f4350041d9a4735dccf9d6d

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
+require "pry"
 def oxford_comma(array)
-
+    
+#binding.pry
+if array.length == 2
+  array.join(" and ")
+elsif array.length < 2
+     array.join 
+elsif array.length > 2
+    last_element = array.pop()
+        array.join(", ") << (", and ") + last_element
+ end
 end
+
